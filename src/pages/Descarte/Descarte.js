6af3f07596b49912035f07ad5982bcc00294c401
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-13 15:31:56 -0300

    Create Descarte.js

---
+++ src/pages/Descarte/Descarte.js
@@ -0,0 +1,232 @@
+import React, { useState, useEffect } from 'react';
+import './Descarte.css';
+import logo from "./grupo.png";
+import axios from "axios";
+
+const Descarte = () => {
+  const [data, setData] = useState("");
+  const [hora, setHora] = useState("");
+  const [loja, setLoja] = useState("");
+  const [produto, setProduto] = useState("");
+  const [departamento, setDepartamento] = useState("");
+  const [quantidade, setQuantidade] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+const handleChange = (event) => {
+  const { name, value } = event.target;
+  if (name === "data") {
+    setData(value);
+  } else if (name === "hora") {
+    setHora(value);
+  } else if (name === "loja") {
+    setLoja(value);
+  } else if (name === "produto") {
+    setProduto(value);
+  } else if (name === "departamento") {
+    setDepartamento(value);
+  } else if (name === "quantidade") {
+    setQuantidade(value);
+  }
+};
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (isSubmitting) {
+      return;
+    }
+
+    setIsSubmitting(true);
+    setErrorMessage("");
+    setSuccessMessage("");
+
+    try {
+      const formData = new FormData();
+      formData.append("Data", data);
+      formData.append("Hora", hora);
+      formData.append("Loja", loja);
+      formData.append("Produto", produto);
+      formData.append("Departamento", departamento);
+      formData.append("Quantidade", quantidade);
+      
+      setIsSending(true);
+
+      await axios.post(
+        "https://script.google.com/macros/s/AKfycbwN-86reWdbhE0_ZW8zK-vA7lU2eLr5L1OIRRT7xGe_DcPx0Hkt9SybVCx-lO4kisgPcA/exec",
+        formData
+      );
+
+      setSuccessMessage("Formulário enviado com sucesso!");
+      setIsSubmitted(true);
+    } catch (error) {
+      setErrorMessage("Ocorreu um erro ao enviar o formulário.");
+    }
+
+    setIsSending(false);
+    setIsSubmitting(false);
+  };
+
+  useEffect(() => {
+    if (isSubmitted) {
+      const timer = setTimeout(() => {
+        setIsSubmitted(false);
+        setData("");
+        setHora("");
+        setLoja("");
+        setProduto("");
+        setDepartamento("");
+        setQuantidade("");
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isSubmitted]);
+  
+    const formatDate = (date) => {
+    const [year, month, day] = date.split("-");
+    return `${day}/${month}/${year}`;
+  };
+  
+    const handleCloseSuccessMessage = () => {
+    setIsSubmitted(false);
+    setSuccessMessage("");
+  };
+  
+    const handleOpenExcelLink = () => {
+    window.open(
+      "https://docs.google.com/spreadsheets/d/18mM2pToUkB7qZBAFzc4T658midBbTaDOxTumtGmU3a0/edit?usp=sharing",
+      "_blank"
+    );
+  };
+  
+    const resetForm = () => {
+    setData("");
+    setHora("");
+    setLoja("");
+    setProduto("");
+    setDepartamento("");
+    setUtilizou("");
+    setQuantidade("");
+  };
+  
+  return (
+    <div className="prevencao">
+      {isSubmitted && successMessage && (
+        <div className="successMessage">
+          <span>{successMessage}</span>
+          <button onClick={handleCloseSuccessMessage}>OK</button>
+        </div>
+      )}    
+      <img src={logo} alt="Logo" className="logo-form" />
+      <h2 className="prevencao__titulo">Formulário de Prevenção</h2>
+      <form className="prevencao__form" onSubmit={handleSubmit}>
+            <h3 className="prevencao__subtitulo">Descartes</h3>
+
+            <div className="prevencao__input-group">
+              <label htmlFor="data">Data:</label>
+              <input
+                type="date"
+                id="data"
+                name="data"
+                value={data}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="prevencao__input-group">
+              <label htmlFor="hora">Hora:</label>
+              <input
+                type="time"
+                id="hora"
+                name="hora"
+                value={hora}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="prevencao__input-group">
+              <label htmlFor="loja">Loja:</label>
+              <select
+                id="loja"
+                name="loja"
+                value={loja}
+                onChange={(event) => setLoja(event.target.value)}
+                required
+              >
+                <option value="">Selecione uma loja</option>
+                <option value="1">Santa Mônica</option>
+                <option value="11">Tomé de Souza</option>
+                <option value="2">Castro Alves</option>
+                <option value="3">Tomba</option>
+                <option value="4">Fraga Maia</option>
+                <option value="5">Artemia Pires</option>
+              </select>
+            </div>
+
+            <div className="prevencao__input-group">
+              <label htmlFor="produto">Produto Furtado:</label>
+              <input
+                type="text"
+                id="produto"
+                name="produto"
+                value={produto}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="prevencao__input-group">
+              <label htmlFor="departamento">Departamento:</label>
+              <select
+                id="departamento"
+                name="departamento"
+                value={departamento}
+                onChange={(event) => setDepartamento(event.target.value)}
+                required
+              >
+                <option value="">Selecione um departamento</option>
+                <option value="Padaria">Padaria</option>
+                <option value="Rotisseria">Rotisseria</option>
+                <option value="Salgados">Salgados</option>
+              </select>
+            </div>
+
+            <div className="prevencao__input-group">
+              <label htmlFor="quantidade">Quantidade de Descarte:</label>
+              <input
+                type="text"
+                id="quantidade"
+                name="quantidade"
+                value={quantidade}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+        <button
+          type="submit"
+          className="prevencao__submit"
+          disabled={isLoading || isSending || isSubmitting}
+        >
+          {isSubmitting ? "Enviando..." : "Enviar"}
+        </button>
+        <button
+          type="button"
+          className="prevencao__avancar"
+          onClick={handleOpenExcelLink}
+        >
+          Lista
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Descarte;
