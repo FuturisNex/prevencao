7fd1719ddaa82a50c0bf384fba6b5b1e98d99792
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-13 10:49:50 -0300

    Update TelaInicial.js

--- src/pages/Inicio/TelaInicial.js
+++ src/pages/Inicio/TelaInicial.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from "./grupo.png";
 import './TelaInicial.css';
 
 function TelaInicial() {
   return (
     <div className="tela-inicial">
       <div className="container">
+          <img src={logo} alt="Logo" className="logo-form" />
         <h1>Prevenção de Perdas</h1>
         <div className="botoes">
           <Link to="/furto" className="botao">
