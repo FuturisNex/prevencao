00cf0f37221a726d176a476c7c66e781cb403940
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-13 15:33:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
 import Prevencao from './pages/prevencao';
 import TelaInicial from './pages/Inicio/TelaInicial';
 import Ocorrencia from './pages/Ocorrencia/Ocorrencia';
+import Descarte from './pages/Descarte/Descarte';
 import NotFound from './pages/NotFound/NotFound';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/furto" element={<Prevencao />} />
           <Route path="/ocorrencia" element={<Ocorrencia />} />
+          <Route path="/descarte" element={<Descarte />} />
           <Route path="/" element={<TelaInicial />} />
             
           <Route path="*" element={<NotFound />} />
