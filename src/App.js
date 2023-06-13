5e13b53fe3f1f0b1892b47e4959f2df927c60673
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-13 10:28:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
 import Prevencao from './pages/prevencao';
-
+import Prevencao from './pages/Inicio/TelaInicial';
+import Prevencao from './pages/Ocorrencia/Ocorrencia';
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <div className='bg-catskillWhite'>
         <Routes>
           <Route path="/" element={<Prevencao />} />
+          <Route path="/ocorrencia" element={<Ocorrencia />} />
+          <Route path="/i" element={<TelaInicial />} />
         </Routes>
       </div>
     </Router>
