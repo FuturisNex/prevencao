17cb1183bed268d7e6b1427f7127414d11f72a4e
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-14 10:47:57 -0300

    Update Descarte.js

--- src/pages/Descarte/Descarte.js
+++ src/pages/Descarte/Descarte.js
@@ -122,10 +122,9 @@ const handleChange = (event) => {
         </div>
       )}    
       <img src={logo} alt="Logo" className="logo-form" />
-      <h2 className="prevencao__titulo">Formulário de Prevenção</h2>
+      <h3 className="prevencao__subtitulo">Descartes</h3>
       <form className="prevencao__form" onSubmit={handleSubmit}>
-            <h3 className="prevencao__subtitulo">Descartes</h3>
-
+        
             <div className="prevencao__input-group">
               <label htmlFor="data">Data:</label>
               <input
