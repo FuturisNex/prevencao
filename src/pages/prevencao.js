153a0f7ecabdff377d604685fa173890af8650cd
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-14 10:48:50 -0300

    Update prevencao.js

--- src/pages/prevencao.js
+++ src/pages/prevencao.js
@@ -181,9 +181,8 @@ const handleChange = (event) => {
         </div>
       )}    
       <img src={logo} alt="Logo" className="logo-form" />
-      <h2 className="prevencao__titulo">Formulário de Prevenção</h2>
+      <h3 className="prevencao__subtitulo">Furto | Degustação</h3>
       <form className="prevencao__form" onSubmit={handleSubmit}>
-            <h3 className="prevencao__subtitulo">Furto | Degustação</h3>
 
             <div className="prevencao__input-group">
               <label htmlFor="nome">Seu Nome:</label>
