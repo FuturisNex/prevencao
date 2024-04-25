2d39396796744cf1ba6f9e2b891316f9de7d86b6
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-04-25 13:33:59 +0000

    main

--- src/pages/Inicio/TelaInicial.js
+++ src/pages/Inicio/TelaInicial.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaEye, FaExclamationCircle, FaToolbox, FaTrashAlt, FaList } from 'react-icons/fa';
 import logo from "../../resources/images/logo.png";
 import './TelaInicial.css';
 
@@ -7,23 +8,23 @@ function TelaInicial() {
   return (
     <div className="tela-inicial">
       <div className="container">
-          <img src={logo} alt="Logo" className="logo-inicio" />
-        <h1 className="titulo">Prevenção de Perdas</h1>
+        <img src={logo} alt="Logo" className="logo-inicial" />
+        <h1 className="titulo-inicio">Prevenção de Perdas</h1>
         <div className="botoes">
-          <Link to="/furto" className="botao">
-            Furtos
+          <Link to="/furto" className="botao-inicio">
+            <FaEye className="icone" /> Furtos
           </Link>
-          <Link to="/ocorrencia" className="botao">
-            Ocorrências
+          <Link to="/ocorrencia" className="botao-inicio">
+            <FaExclamationCircle className="icone" /> Ocorrências
           </Link>
-          <Link to="/equipamentos" className="botao">
-            Equipamentos
+          <Link to="/equipamentos" className="botao-inicio">
+            <FaToolbox className="icone" /> Equipamentos
           </Link>
-          <Link to="/descarte" className="botao">
-            Descartes
+          <Link to="/descarte" className="botao-inicio">
+            <FaTrashAlt className="icone" /> Descartes
           </Link>
-              <Link to="/inventario" className="botao1">
-            Inventário
+          <Link to="/inventario" className="botao-inicio">
+            <FaList className="icone" /> Inventário
           </Link>
         </div>
       </div>
